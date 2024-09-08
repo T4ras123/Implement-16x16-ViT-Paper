2ea42fc20701b96f132cede5f5350edbc588c562
T4ras123 <vovatara123@gmail.com>
2024-09-08 12:58:04 +0400

    ffj

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 transform = 3
 
 trainset = datasets.ImageNet(root='./data', train=True, download=True, transform=transform)
-trainloader = DataLoader(trainset, batch_size=64, shuffle=True)
+trainloader = DataLoader(trainset, batch_size=128, shuffle=True)
 
 testset = datasets.ImageNet(root='./data', train=False, download=True, transform=transform)
-testloader = DataLoader(testset, batch_size=64, shuffle=False)
+testloader = DataLoader(testset, batch_size=128, shuffle=False)
