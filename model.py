8b8b34f608fbd3cfb32e4ae0b877dd7ff72f348a
T4ras123 <vovatara123@gmail.com>
2024-09-08 12:06:33 +0400

    load model.py

---
+++ model.py
@@ -0,0 +1,14 @@
+import torch 
+import torch.nn as nn
+import torchvision
+from torchvision import datasets
+from torch.utils.data import Dataset, DataLoader
+import matplotlib.pyplot as plt 
+
+transform = 3
+
+trainset = datasets.ImageNet(root='./data', train=True, download=True, transform=transform)
+trainloader = DataLoader(trainset, batch_size=64, shuffle=True)
+
+testset = datasets.ImageNet(root='./data', train=False, download=True, transform=transform)
+testloader = DataLoader(testset, batch_size=64, shuffle=False)
